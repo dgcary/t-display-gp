c788c7e250d5ec7e9d99506effca8eedca948217
dgcary <82438976+dgcary@users.noreply.github.com>
2026-08-19 12:12:09 +0800

    feat: add weather screen

---
+++ src/ui/WeatherScreen.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "WeatherController.h"
+
+class TFT_eSPI;
+class U8g2_for_TFT_eSPI;
+
+class WeatherScreen {
+ public:
+  void begin(TFT_eSPI& display, U8g2_for_TFT_eSPI& unicodeFont);
+  void render(const WeatherViewModel& model, bool fullRedraw);
+
+ private:
+  TFT_eSPI* display_ = nullptr;
+  U8g2_for_TFT_eSPI* unicodeFont_ = nullptr;
+};
